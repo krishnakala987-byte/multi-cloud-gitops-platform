19c65b82550a68f3310297d3ddd2730dbddeeb55
Krishna Kala <YOUR_GITHUB_EMAIL>
2026-07-24 04:46:41 +0000

    modified:   github-oidc.tf

--- terraform/aws/github-oidc.tf
+++ terraform/aws/github-oidc.tf
@@ -21,6 +21,7 @@ data "aws_iam_policy_document" "github_assume" {
       variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
+
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
